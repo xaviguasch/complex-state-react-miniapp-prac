162ee8083e24168e709d1759a065a2a1a9609c0f
xaviguasch <xaviguasch@gmail.com>
2020-01-01 21:42:14 +0100

    solved challenge exercise

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react'
 
 function App() {
-    const [fullName, setFullName] = useState({
+    const [contact, setContact] = useState({
         fName: '',
-        lName: ''
+        lName: '',
+        email: ''
     })
 
     function handleChange(event) {
         const { value, name } = event.target
 
-        setFullName(prevValue => {
+        setContact(prevValue => {
             if (name === 'fName') {
                 return {
                     fName: value,
-                    lName: prevValue.lName
+                    lName: prevValue.lName,
+                    email: prevValue.email
                 }
             } else if (name === 'lName') {
                 return {
                     fName: prevValue.fName,
-                    lName: value
+                    lName: value,
+                    email: prevValue.email
+                }
+            } else if (name === 'email') {
+                return {
+                    fName: prevValue.fName,
+                    lName: prevValue.lName,
+                    email: value
                 }
             }
         })
@@ -27,20 +36,27 @@ function App() {
     return (
         <div className='container'>
             <h1>
-                Hello {fullName.fName} {fullName.lName}
+                Hello {contact.fName} {contact.lName}
             </h1>
+            <p>{contact.email}</p>
             <form>
                 <input
                     onChange={handleChange}
                     name='fName'
                     placeholder='First Name'
-                    valuee={fullName.fName}
+                    valuee={contact.fName}
                 />
                 <input
                     onChange={handleChange}
                     name='lName'
                     placeholder='Last Name'
-                    valuee={fullName.lName}
+                    valuee={contact.lName}
+                />
+                <input
+                    onChange={handleChange}
+                    name='email'
+                    placeholder='Email'
+                    valuee={contact.email}
                 />
                 <button>Submit</button>
             </form>
@@ -49,3 +65,55 @@ function App() {
 }
 
 export default App
+
+// import React, { useState } from 'react'
+
+// function App() {
+//     const [fullName, setFullName] = useState({
+//         fName: '',
+//         lName: ''
+//     })
+
+//     function handleChange(event) {
+//         const { value, name } = event.target
+
+//         setFullName(prevValue => {
+//             if (name === 'fName') {
+//                 return {
+//                     fName: value,
+//                     lName: prevValue.lName
+//                 }
+//             } else if (name === 'lName') {
+//                 return {
+//                     fName: prevValue.fName,
+//                     lName: value
+//                 }
+//             }
+//         })
+//     }
+
+//     return (
+//         <div className='container'>
+//             <h1>
+//                 Hello {fullName.fName} {fullName.lName}
+//             </h1>
+//             <form>
+//                 <input
+//                     onChange={handleChange}
+//                     name='fName'
+//                     placeholder='First Name'
+//                     valuee={fullName.fName}
+//                 />
+//                 <input
+//                     onChange={handleChange}
+//                     name='lName'
+//                     placeholder='Last Name'
+//                     valuee={fullName.lName}
+//                 />
+//                 <button>Submit</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default App

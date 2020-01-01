e2a8b9f9ba24c6e68b51024c3e88669f7b1a8c2f
xaviguasch <xaviguasch@gmail.com>
2020-01-01 21:07:52 +0100

    small refactor with object destructuring

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,18 @@ function App() {
     })
 
     function handleChange(event) {
-        const newValue = event.target.value
-        const inputName = event.target.name
+        const { value, name } = event.target
 
         setFullName(prevValue => {
-            if (inputName === 'fName') {
+            if (name === 'fName') {
                 return {
-                    fName: newValue,
+                    fName: value,
                     lName: prevValue.lName
                 }
-            } else if (inputName === 'lName') {
+            } else if (name === 'lName') {
                 return {
                     fName: prevValue.fName,
-                    lName: newValue
+                    lName: value
                 }
             }
         })
